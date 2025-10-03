64f0d145179efe9ee980b44dec43efa751b10ea4
housemouse62 <ryan.skeels@gmail.com>
2025-10-03 20:06:10 +0200

    Added classes. Separated UI from Book Creation. Toggling read/unread only updates icon, removing book only removes book, no more rerendering page.

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const myLibrary = [];
+
 const container = document.querySelector("#container");
 const newTitle = document.querySelector("#newTitle");
 const newAuthor = document.querySelector("#newAuthor");
 const newPages = document.querySelector("#newPages");
 const readIt = document.querySelector("#readIt");
 const addBookButton = document.querySelector(".addBook"); 
-let card;
-let data;
 
 let html = '';
 
@@ -21,72 +19,65 @@ class Book {
     this.readIt = readIt;
     this.id = id;
 };
+
+  changeReadStatus() {
+    this.readIt = !this.readIt;
+}
 };
 
-if (myLibrary.length === 0) {
-addBookToLibrary("Diary of a Wimpy Kid", "Jeff Kinney", 224, true);
-addBookToLibrary("Jurassic Park", "Michael Crichton", 464, false);
-
-};
-
-addBookButton.addEventListener('click', (event) => {
-    console.log(readIt.checked)
-  addBookToLibrary(newTitle.value, newAuthor.value, newPages.value, readIt.checked);
+addBookButton.addEventListener('submit', (event) => {
+  library.addBookToLibrary(newTitle.value, newAuthor.value, newPages.value, readIt.checked);
   
-  displayBooks();
+  ui.displayBooks(library.books);
   event.preventDefault();
 });
 
-// Add Book to myLibrary Array
-function addBookToLibrary(title, author, pages, readIt) {
-  id = crypto.randomUUID();
-  const newBook = new Book(title, author, pages, readIt, id);
-  myLibrary.push(newBook);
+// Add Book to library Array
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+    addBookToLibrary(title, author, pages, readIt, id = crypto.randomUUID()) {
+      const newBook = new Book(title, author, pages, readIt, id);
+      this.books.push(newBook)
+  }
 };
+class UI {
+  constructor(){}
+  
+  displayBooks(bookArray = []) {
+    container.innerHTML = '';
 
-// Display Book
-function displayBooks() {
-  container.innerHTML = '';
+    bookArray.forEach(item => {
+      const card = document.createElement('div');
+      card.classList = 'book';
 
-  myLibrary.forEach(item => {
-    card = document.createElement('div');
-    card.classList = 'book';
+      const topBlock = document.createElement('div');
+      topBlock.className = 'topBlock';
+      card.appendChild(topBlock);
 
-    const topBlock = document.createElement('div');
-    topBlock.className = 'topBlock';
-    card.appendChild(topBlock);
+      const title = document.createElement('h4');
+      title.textContent = `${item.title}`;
+      topBlock.appendChild(title);
 
-    const title = document.createElement('h4');
-    title.textContent = `${item.title}`;
-    topBlock.appendChild(title);
+      const author = document.createElement('h5');
+      author.textContent = `${item.author}`;
+      topBlock.appendChild(author);
 
-    const author = document.createElement('h5');
-    author.textContent = `${item.author}`;
-    topBlock.appendChild(author);
+      const pages = document.createElement('h5');
+      if (item.pages !== '') {
+      pages.textContent = `${item.pages} pgs`;};
+      topBlock.appendChild(pages);
 
-    const pages = document.createElement('h5');
-    if (item.pages !== '') {
-    pages.textContent = `${item.pages} pgs`;};
-    topBlock.appendChild(pages);
+      const bottomBlock = document.createElement('div');
+      bottomBlock.className = 'bottomBlock';
+      card.appendChild(bottomBlock);
 
-    
-    
-    
-
-    const bottomBlock = document.createElement('div');
-    bottomBlock.className = 'bottomBlock';
-    card.appendChild(bottomBlock);
-
-    const readIcon = document.createElement('img');
-    readIcon.className = 'readIcon';
-    if (item.readIt === true) {
-      readIcon.src = "./Images/checkbox-marked-outline.svg";
-    } else {
-      readIcon.src = "./Images/checkbox-blank-outline.svg";
-    //const readIt = document.createElement('h5');
-    //readIt.textContent = 'Read It';
-    };
-     bottomBlock.appendChild(readIcon);
+      const readIcon = document.createElement('img');
+      readIcon.className = 'readIcon';
+      readIcon.src = item.readIt ? "./Images/checkbox-marked-outline.svg" :  "./Images/checkbox-blank-outline.svg";
+      bottomBlock.appendChild(readIcon)
 
     const removeButton = document.createElement('button');
     removeButton.innerText = 'Remove';
@@ -95,15 +86,21 @@ function displayBooks() {
     const toggleStatus = document.createElement('button');
     toggleStatus.innerText = 'Read';
     toggleStatus.className = 'readButton';
-    toggleStatus.addEventListener("click", (event) => item.changeReadStatus());
+    toggleStatus.addEventListener("click", (event) => {item.changeReadStatus()
+      readIcon.src = item.readIt ? 
+        "./Images/checkbox-marked-outline.svg" :
+        "./Images/checkbox-blank-outline.svg"
+      })
     bottomBlock.appendChild(toggleStatus);
 
     removeButton.addEventListener("click", (event) => {
-    const findIt = myLibrary.map(item => item.id).indexOf(removeButton.dataset.id);
-    console.log(findIt);
-    myLibrary.splice(findIt, 1);
-
-    displayBooks();
+    const findIt = bookArray.map(item => item.id).indexOf(removeButton.dataset.id);
+    bookArray.splice(findIt, 1);
+    
+    const removeBook = document.querySelector(`[data-id="${removeButton.dataset.id}"]`);
+    if (removeBook) {
+      removeBook.remove()
+    }
   });
     
     card.setAttribute("data-id", item.id);
@@ -112,17 +109,16 @@ function displayBooks() {
     removeButton.setAttribute("data-id", item.id);
     bottomBlock.appendChild(removeButton);
   })};
+};
 
- displayBooks();
+const library = new Library();
+const ui = new UI();
 
-Book.prototype.changeReadStatus = function() {
-  if (this.readIt === true) {
-    this.readIt = false
-  } else {
-    this.readIt = true
-  };
-  displayBooks();
-}
-const removeButton = document.querySelectorAll(".removeButton");
+if (library.books.length === 0) {
+  library.addBookToLibrary("Diary of a Wimpy Kid", "Jeff Kinney", 224, true);
+  library.addBookToLibrary("Jurassic Park", "Michael Crichton", 464, false);
 
+};
+
+ui.displayBooks(library.books);
   
